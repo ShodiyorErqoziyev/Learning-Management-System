8fb6bd2cf9632684987730fada94a6a6eeae960d
Erqoziyev <erqoziyevshodiyor@gmail.com>
2024-01-11 22:50:33 +0500

    Added Darkmode

---
+++ LearningManagementSystem/LearningManagementSystem/Themes/AppTheme.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows;
+
+namespace LearningManagementSystem.Themes;
+
+public class AppTheme
+{
+    public static void ChangeTheme(Uri themeuri)
+    {
+        ResourceDictionary Theme = new ResourceDictionary() { Source = themeuri };
+
+        App.Current.Resources.Clear();
+        App.Current.Resources.MergedDictionaries.Add(Theme);
+    }
+}
--- LearningManagementSystem/LearningManagementSystem/Windows/MainWindow.xaml.cs
+++ LearningManagementSystem/LearningManagementSystem/Windows/MainWindow.xaml.cs
@@ -1,18 +1,8 @@
 ﻿using LearningManagementSystem.Pages;
+using LearningManagementSystem.Themes;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace LearningManagementSystem
 {
@@ -79,6 +69,19 @@ namespace LearningManagementSystem
         {
             Groups groups = new Groups();
             PageNavigator.Content = groups;
+        }
+
+        private void IsChecked(object sender, RoutedEventArgs e)
+        {
+            if (chkbox.IsChecked == true)
+            {
+                AppTheme.ChangeTheme(new Uri("Themes/DarkTheme.xaml", UriKind.Relative));
+
+            }
+            else
+            {
+                AppTheme.ChangeTheme(new Uri("Themes/LightTheme.xaml", UriKind.Relative));
+            }
         }
 
         private void tg_btn_menu_Click(object sender, RoutedEventArgs e)
